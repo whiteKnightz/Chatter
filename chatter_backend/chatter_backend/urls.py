ce5c8cfade3cfcce038267c2ad2ca9c19daf93e7
Abhishek Satyal <ab.satyal@gmail.com>
2022-04-22 00:05:40 +0545

    fix: fix fetch chat by user

--- chatter_backend/chatter_backend/urls.py
+++ chatter_backend/chatter_backend/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('api/auth/login/', views.Login.as_view()),
     path('api/auth/signup/', views.Signup.as_view()),
     path('api/user/', views.UserList.as_view()),
-    path('api/chat/', views.ChatApi.as_view()),
     path('api/chat/<str:chat_id>/', views.ChatApi.as_view()),
     path('api/chat/search/byPerson/', views.ChatByPersonsApi.as_view()),
     path('api/chat/search/byPerson/<str:username>/', views.ChatByPersonsApi.as_view()),
