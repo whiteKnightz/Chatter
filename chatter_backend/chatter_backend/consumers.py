1d2be2ceb3935ca033cbf48bda10140faa32f058
Abhishek Satyal <ab.satyal@gmail.com>
2022-04-23 13:59:50 +0545

    fix: fix web socket channel in backed

--- chatter_backend/chatter_backend/consumers.py
+++ chatter_backend/chatter_backend/consumers.py
@@ -1,6 +1,9 @@
 import json
+import random
+
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+import datetime
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -15,27 +18,40 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        correspondence = text_data_json['correspondence']
+        correspondence['created_date'] = str(datetime.datetime.now())
         chat_name = text_data_json['chat_name']
+        chat = text_data_json['chat']
+        print(chat['gcs'])
+        gcs = list(chat['gcs'])
+        chat_value = chat['chat']
+        del chat['gcs']
+        gcs.append(correspondence)
+        chat_value['chats'] = gcs
+        chat_value['chat_id'] = str(random.randint(1000000, 10000000)) if chat_value['chat_id'] is None else chat_value[
+            'chat_id']
 
-        print(f'\n\n\nMessage is: \"{message}\"')
+        print(f'\n\n\nMessage is: \"{correspondence}\"')
         print(f'\n\n\nChat Name is: \"{chat_name}\"')
+        print(f'\n\n\nChat is: \"{chat}\"')
+        print(f'\n\n\nchat_value is: \"{chat_value}\"')
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message,
-                'chat_name': chat_name
+                'message': correspondence,
+                'chat_name': chat_name,
+                'chat': chat_value
             }
         )
 
     def chat_message(self, event):
-        message = event['message']
         chat_name = event['chat_name']
+        chat = event['chat']
 
         self.send(text_data=json.dumps({
             'type': 'chat',
-            'message': message,
-            'chat_name': chat_name
+            'chat_name': chat_name,
+            'chat': chat
         }))
